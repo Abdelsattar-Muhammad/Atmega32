8ffb9d802304712deeb82e3d523f84d3453a1775
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-05-01 18:29:03 +0300

    Add files via upload

--- HAL/Servo Motor/Servo Motor.c
+++ HAL/Servo Motor/Servo Motor.c
@@ -1,1 +1,21 @@
-
+﻿/*
+ * Servo_Motor.c
+ *
+ * Created: 01/05/2023 03:53:58 ص
+ *  Author: Abdelsattar
+ */ 
+#include "Servo Motor.h"
+void Servo_motor(s8 angle)
+{
+	d32 val;
+	if (angle>0)
+	{
+		val=((0.5/90)*angle)+1.5;
+		TIMER1_FastPWM(val);
+	} 
+	else
+	{
+		val=1.5+((0.5/90)*angle);
+		TIMER1_FastPWM(val);
+	}
+}
---
+++ HAL/Servo Motor/Servo Motor.h
@@ -0,0 +1,17 @@
+﻿/*
+ * Servo_Motor.h
+ *
+ * Created: 01/05/2023 04:24:53 م
+ *  Author: Abdelsattar
+ */ 
+
+
+#ifndef SERVO MOTOR_H_
+#define SERVO MOTOR_H_
+#include "data_types.h"
+#include "TIMER.h"
+
+void Servo_motor(s8 angle);
+
+
+#endif /* SERVO MOTOR_H_ */
