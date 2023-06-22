6269dcee8da1d188d669858a8be340ad524ce60f
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-06-22 19:01:16 +0300

    Add files via upload

---
+++ 01 LIB/DATA_TYPES.h
@@ -0,0 +1,22 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V01					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+
+
+#ifndef DATA_TYPES_H_
+#define DATA_TYPES_H_
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
+typedef signed char s8;
+typedef signed short s16;
+typedef signed long s32;
+typedef signed long long s64;
+typedef float f32;
+typedef double f64;
+
+#endif /* DATA_TYPES_H_ */
