30100c90b0a9b513602185ca04d96e35738420dd
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-06-22 19:04:54 +0300

    Add files via upload

---
+++ MCAL/01 DIO/DIO_config.h
@@ -0,0 +1,17 @@
+/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 20 June 2023            */
+/* Version : V01					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+
+/* Empty */ 
+
+
+#endif
+
+
--- MCAL/01 DIO/DIO_interface.h
+++ MCAL/01 DIO/DIO_interface.h
@@ -1,1 +1,60 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 20 June 2023            */
+/* Version : V01					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+#ifndef DIO_INTERFACE_H_
+#define DIO_INTERFACE_H_
 
+
+#define LOW 0
+#define HIGH 1
+
+typedef enum
+{
+ INPUT=0,
+ OUTPUT=1
+
+}DIR_t;
+
+
+typedef enum
+{
+ PIN0=0,
+ PIN1=1,
+ PIN2=2,
+ PIN3=3,
+ PIN4=4,
+ PIN5=5,
+ PIN6=6,
+ PIN7=7,
+}PINS_t;
+typedef enum
+{
+ APORT=0,
+ BPORT=1,
+ CPORT=2,
+ DPORT=3,
+}PORT_t;
+
+void DIO_vSetPinDirection(PORT_t Copy_PortName,PINS_t Copy_PinNumber,DIR_t Copy_Direction );
+
+void DIO_vSetPinValue(PORT_t Copy_PortName,PINS_t Copy_PinNumber,u8 value);
+
+u8	 DIO_u8GetPinValue(PORT_t Copy_PortName,PINS_t Copy_PinNumber);
+
+void DIO_vTogglePin(PORT_t Copy_PortName,PINS_t Copy_PinNumber);
+
+void DIO_vSetPullupPin(PORT_t Copy_PortName,PINS_t Copy_PinNumber);
+
+
+
+void DIO_vSetPortDirection(PORT_t Copy_PortName,DIR_t Copy_Direction );
+
+void DIO_vSetPortValue(PORT_t Copy_PortName,u8 value);
+
+u8 DIO_u8GetPortValue(PORT_t Copy_PortName);
+
+
+#endif
