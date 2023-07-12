27ee239a95a4fc2d7c48057a39f17e9beddaff1c
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-07-12 20:51:40 +0300

    Add files via upload

--- 03 HAL/01 LED/LED_interface.h
+++ 03 HAL/01 LED/LED_interface.h
@@ -1,1 +1,16 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
 
+#ifndef LED_INTERFACE_H
+#define LED_INTERFACE_H
+
+void LED_Init(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+void LED_TurnOn(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+void LED_TurnOff(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+void LED_Toggle(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+
+#endif /* LED_H_ */
---
+++ 03 HAL/01 LED/LED_program.c
@@ -0,0 +1,33 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+#include "DATA_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+#include "LED_interface.h"
+
+/*initialization pin as output at given port*/
+void LED_Init(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vSetPinDirection(Copy_u8PortName,Copy_u8PinNumber,OUTPUT);
+	
+}
+
+void LED_TurnOn(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vSetPinValue(Copy_u8PortName,Copy_u8PinNumber,HIGH);
+}
+
+void LED_TurnOff(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vSetPinValue(Copy_u8PortName,Copy_u8PinNumber,LOW);
+}
+
+void LED_Toggle(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vTogglePin(Copy_u8PortName,Copy_u8PinNumber);
+}
