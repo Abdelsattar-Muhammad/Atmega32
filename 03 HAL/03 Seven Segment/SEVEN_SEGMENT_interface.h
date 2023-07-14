5e7d5010ecdaf44119db8e463359e9c59210ddde
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-07-14 16:30:15 +0300

    Add files via upload

---
+++ 03 HAL/03 Seven Segment/SEVEN_SEGMENT_interface.h
@@ -0,0 +1,34 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 29 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+
+#ifndef SEVEN_SEGMENT_INTERFACE_H
+#define SEVEN_SEGMENT_INTERFACE_H
+#define  COMMON_CATHOD
+
+typedef enum {
+	ZERO =0b00111111,
+	ONE  =0b00000110,
+	TWO  =0b01011011,
+	THREE=0b01001111,
+	FOUR =0b01100110,
+	FIVE =0b01101101,
+	SIX  =0b01111101,
+	SEVEN=0b00000111,
+	EIGHT=0b01111111,
+	NINE =0b01101111,
+	DOT  =0b10000000,
+}Numbers_SS;
+
+void SEVEN_SEG_Init(PORT_t Copy_PortName);
+void SEVEN_SEG_Write(PORT_t Copy_PortName,u8 Copy_u8Number);
+void SEVEN_SEG_BCD_Init(PORT_t Copy_PortName);
+
+void SEVEN_SEG_BCD_Write(PORT_t Copy_PortName,u8 Copy_u8Number);
+
+
+
+#endif 
---
+++ 03 HAL/03 Seven Segment/SEVEN_SEGMENT_ptogram.c
@@ -0,0 +1,49 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 29 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+
+#include "DATA_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO_interface.h"
+
+#include "SEVEN_SEGMENT_interface.h"
+	
+
+void SEVEN_SEG_Init(PORT_t Copy_PortName)
+{
+	DIO_vSetPortDirection(Copy_PortName,0xFF);
+}
+
+
+void SEVEN_SEG_Write(PORT_t Copy_PortName,u8 Copy_u8Number)
+{
+	u8 arr[10]={ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
+		
+	#if defined COMMON_CATHOD
+	DIO_vSetPortValue(Copy_PortName,arr[Copy_u8Number]);
+	#elif defined COMMON_ANODE 
+	DIO_vSetPortValue(Copy_PortName,~arr[Copy_u8Number]);
+	#endif
+}
+
+void SEVEN_SEG_BCD_Init(PORT_t Copy_PortName)//low niblle
+{
+	DIO_vSetPinDirection(Copy_PortName,Pin0,OUTPUT);
+	DIO_vSetPinDirection(Copy_PortName,Pin1,OUTPUT);
+	DIO_vSetPinDirection(Copy_PortName,Pin2,OUTPUT);
+	DIO_vSetPinDirection(Copy_PortName,Pin3,OUTPUT);
+}
+
+
+void SEVEN_SEG_BCD_Write(PORT_t Copy_PortName,u8 Copy_u8Number)
+{
+	DIO_vSetPinValue(Copy_PortName,Pin0,GET_BIT(Copy_u8Number,0));
+	DIO_vSetPinValue(Copy_PortName,Pin1,GET_BIT(Copy_u8Number,1));
+	DIO_vSetPinValue(Copy_PortName,Pin2,GET_BIT(Copy_u8Number,2));
+	DIO_vSetPinValue(Copy_PortName,Pin3,GET_BIT(Copy_u8Number,3));
+}
+
+
