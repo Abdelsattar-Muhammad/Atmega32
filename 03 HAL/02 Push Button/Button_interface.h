b75a7ea933e84b343a5c996faca517f8ed302695
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-07-14 16:27:30 +0300

    Push Button

--- 03 HAL/02 Push Button/Button_interface.h
+++ 03 HAL/02 Push Button/Button_interface.h
@@ -1,1 +1,19 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
 
+#ifndef BUTTON_INTERFACE_H
+#define BUTTON_INTERFACE_H
+
+void Button_Init(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+
+void Button_PullupInit(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+u8 	 Button_u8Read(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber);
+
+
+
+
+#endif 
---
+++ 03 HAL/02 Push Button/Button_program.c
@@ -0,0 +1,27 @@
+﻿/*************************************/
+/* Author  : Abdelsattar Muhammad    */
+/* Date    : 22 June 2023            */
+/* Version : V02					*/
+/* MCU 	   : ATMEGA32               */
+/************************************/
+#include "DATA_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+#include "Button_interface.h"
+
+void Button_Init(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vSetPinDirection(Copy_u8PortName,Copy_u8PinNumber,INPUT);
+}
+void Button_PullupInit(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	DIO_vSetPullupPin(Copy_u8PortName,Copy_u8PinNumber);
+	
+}
+u8 	 Button_u8Read(PORT_t Copy_u8PortName,PINS_t Copy_u8PinNumber)
+{
+	u8 Local_u8Value=0;
+	Local_u8Value=DIO_u8GetPinValue(Copy_u8PortName,Copy_u8PinNumber);
+	return Local_u8Value;
+}
